0e194b4f27f5fe4c715bd306fcd09a1fab473ce4
Jaroslaw Korbut <korbutf1.j@gmail.com>
2021-04-03 21:08:02 +0200

    task three

---
+++ JMRTech/JMTech.TaskThree/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JMTech.TaskThree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var test = new List<IEnumerable<string>>()
+            {
+                new List<string>()
+                {
+                    ":", ";", "."
+                },
+                new List<string>()
+                {
+                    ":", ";", ".", ";", ".", ";", ".", ";", "."
+                },
+                new List<string>()
+                {
+                    ":", ";", ".", ";", ".", ";", ".", ";", ".", ";", ".", ";", "."
+                },
+                new List<string>()
+                {
+                    ":", ";", "."
+                },
+            };
+
+            Console.WriteLine(OnlyBigCollections(test).Count());
+        }
+
+        public static IEnumerable<IEnumerable<string>> 
+            OnlyBigCollections(List<IEnumerable<string>> toFilter)
+        {
+            Func<IEnumerable<string>, bool> predicate =
+                    list => 
+                    {
+                        var enumerator = list.GetEnumerator();
+                        for (int i = 0; i < 6; i++) 
+                        {
+                            if(!enumerator.MoveNext())
+                            {
+                                return false;
+                            }
+                        }
+                        return true;
+                    };
+
+            return toFilter.Where(predicate);
+        }
+
+    }
+}
